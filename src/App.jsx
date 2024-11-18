7e937b728987e7c41d3c4f41fa6a6478b697d9bf
jh_lee <juhyean8723@gmail.com>
2024-11-18 17:28:51 +0900

    페이지 이동 방법 및 동적 경로

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,39 @@
       2. main.jsx
         - BrowserRouter import 후 <App /> 컴포넌트 감싸기
 
-      3. App.jsx 
-        - <Routes>, <Route> 컴포넌트 import
-        - uri, 컴포넌트 설정 (path, element 속성)
-       
-        ※ path 설정 시 와일드카드: * (path에 해당하는 uri 없으면 지정할 컴포넌트)
+      3. 페이지 이동 사용방법
+          * 1. Routes, Route 컴포넌트 사용
+              - uri, 컴포넌트 설정 (path, element 속성)
+              - Routes 컴포넌트 내부에는 Route 컴포넌트만 사용 가능
+
+              ※ path 설정 시 와일드카드: * (path에 해당하는 uri 없으면 지정할 컴포넌트)
+
+              ex) <Routes>
+                    <Route path="/diary/:id" uri={</Diary>} /> // :id - url parameter
+                  </Routes>
+              
+              ※ 동적 파라미터
+                  - URL Parameter : useParams 훅 사용
+                    ex) /diary/1
+                        const params = useParams();
+                        console.log(params.id);
+
+                  - Query String  : useSearchParams 휵 사용
+                    ex) /diary?id=1
+                        const [params, setParams] = useSearchParams();
+                        console.log(params.get('id'));                        
+
+          * 2. Link 컴포넌트 사용
+              - to 속성에 경로 설정
+
+              ex) <Link to={"/"}>Home</Link>
+
+
+          * 3. useNavigate 훅 사용
+              - 특정 이벤트 시 함수 호출 방식으로 사용 (버튼 등)
+              
+              ex) const nav = useNavigate();
+                  const onClick = () => { nav("/") }
 */
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
