f53d46011fd0db7caaa913db4f558cea2433b733
jh_lee <juhyean8723@gmail.com>
2024-11-19 16:11:11 +0900

    생성, 수정, 삭제 메서드 및 컨텍스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,77 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
-import { getEmotionImage } from "./util/get-emotion-image";
 import Header from "./components/Header";
 import Button from "./components/Button";
+import { createContext, useReducer, useRef } from "react";
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELTE":
+      return state.filter((item) => String(item.id) !== String(action.id));
+  }
+  return state;
+}
+
+const diaryStateContext = createContext();
+const diaryDispatchContext = createContext();
 
 function App() {
-  const nav = useNavigate();
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
 
-  const onClickButton = () => {
-    nav("/new");
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    });
   };
 
   return (
@@ -22,13 +83,17 @@ function App() {
         leftChild={<Button text={"Left"} />}
         rightChild={<Button text={"Right"} />}
       />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+      <diaryStateContext.Provider value={data}>
+        <diaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </diaryDispatchContext.Provider>
+      </diaryStateContext.Provider>
     </>
   );
 }
---
+++ src/pages/Edit.jsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+const Edit = () => {
+  const parmas = useParams();
+  return <div>Edit</div>;
+};
+
+export default Edit;
