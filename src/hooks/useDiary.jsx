9d01ba7a8f57288cc5b51a940ceaeaf939ea30c4
jh_lee <juhyean8723@gmail.com>
2024-11-20 16:53:17 +0900

    Diary 페이지 구현

---
+++ src/hooks/useDiary.jsx
@@ -0,0 +1,33 @@
+import { useContext, useState, useEffect } from "react";
+import { DiaryStateContext } from "../App";
+import { useNavigate } from "react-router-dom";
+
+const useDiary = (id) => {
+  const data = useContext(DiaryStateContext);
+  const [curDiaryItem, setCurDiaryItem] = useState();
+  const nav = useNavigate();
+
+  /* 
+    * useNavigate 훅
+      - 컴포넌트가 마운트되기 전에는 동작하지 않는다.
+      - 이벤트 헨들러에서 사용할 때는 이미 마운트가 된 후 이벤트가 동작하는 것이라 관계 X
+      - 컴포넌트 안에서 함수로 작성하게 되면, 항상 마운트되기 전 이므로 동작하지 않기 때문에
+        useEffect 훅을 사용하여 마운트 이후에 동작하도록 구현해야 한다.
+  */
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(id)
+    );
+
+    if (!currentDiaryItem) {
+      window.alert("존재하지 않는 일기입니다.");
+      nav("/", { replace: true });
+    }
+
+    setCurDiaryItem(currentDiaryItem);
+  }, [id, data]); // 마운트된 후 와 params.id, data 가 변경될 때마다 동작
+
+  return curDiaryItem;
+};
+
+export default useDiary;
--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,5 +1,40 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
+import { useNavigate, useParams } from "react-router-dom";
+import useDiary from "../hooks/useDiary";
+import { getStringedDate } from "../util/get-stringed-date";
+
 const Diary = () => {
-  return <div>Diary</div>;
+  const params = useParams();
+  const nav = useNavigate();
+
+  /*  
+    커스텀 훅인 useDiary로 반환받은 curDiaryItem에 처음에는 undefined가 들어간다.
+    왜냐하면 useEffect안에 현재 다이어리 데이터를 세팅해주는 코드는 마운트가된 이후에 실행되기에
+    처음 마운트되기전 실행되는 코드에는 세팅 값이 들어가지 않기 때문이다.
+  */
+  const curDiaryItem = useDiary(params.id);
+
+  if (!curDiaryItem) {
+    return <div>데이터 로딩중...!</div>;
+  }
+
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
+
+  return (
+    <div>
+      <Header
+        title={title}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/Edit/${params.id}`)} text={"수정하기"} />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
 };
 
 export default Diary;
